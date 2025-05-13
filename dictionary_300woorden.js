b00fc48d02bcfbaa4230b9016c89583d8258c6d2
iliassA17 <64663067+iliassA17@users.noreply.github.com>
2025-05-13 02:43:35 +0200

    Add files via upload

---
+++ dictionary_300woorden.js
@@ -0,0 +1,14 @@
+const dictionary = {
+  "salam": "hallo",
+  "hallo": "salam",
+  "labas": "hoe gaat het?",
+  "hoe gaat het?": "labas",
+  "shukran": "dank je",
+  "dank je": "shukran",
+  "bslama": "tot ziens",
+  "tot ziens": "bslama",
+  "kif dayr?": "hoe gaat het met je?",
+  "hoe gaat het met je?": "kif dayr?",
+  "fiyya jou3": "ik heb honger",
+  "ik heb honger": "fiyya jou3",
+};
